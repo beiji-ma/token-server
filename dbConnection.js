0997d74f6b7e94286038aef0e98f1927305df486
Beiji Ma <beiji.ma@outlook.com>
2021-09-18 12:41:52 +0200

    changes for enabling docker

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql')
 
 const connnection = mysql.createConnection({
-  host: '127.0.0.1',
+  host: process.env.DATABASE_HOST || '127.0.0.1',
+  port: '3306',
   user: 'root',
   password: '123456',
   database: 'auth'
@@ -9,7 +10,11 @@ const connnection = mysql.createConnection({
 
 
 connnection.connect(function (err) {
-  if (err) throw err
+  //if (err) throw err
+  if (err) {
+	  console.log(err)
+	  throw err
+  }
   console.log('connected.')
 })
 
--- scripts/database.sql
+++ scripts/database.sql
@@ -1,4 +1,6 @@
-CREATE TABLE `users` (
+CREATE DATABASE IF NOT EXISTS auth;
+USE auth;
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -6,3 +8,5 @@ CREATE TABLE `users` (
   PRIMARY KEY (id),
   UNIQUE KEY email (email)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+INSERT INTO users(name,email,password) VALUES('Tom', 'lyxca@hotmail.com', '123456');
